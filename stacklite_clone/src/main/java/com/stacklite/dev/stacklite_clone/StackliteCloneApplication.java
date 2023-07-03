8d2cfdf3efd54d33748d1b6665edc5b8132115cc
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-07-03 09:29:31 +0100

    project initialization

---
+++ stacklite_clone/src/main/java/com/stacklite/dev/stacklite_clone/StackliteCloneApplication.java
@@ -0,0 +1,13 @@
+package com.stacklite.dev.stacklite_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackliteCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackliteCloneApplication.class, args);
+	}
+
+}
---
+++ stacklite_clone/src/test/java/com/stacklite/dev/stacklite_clone/StackliteCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacklite.dev.stacklite_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackliteCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
